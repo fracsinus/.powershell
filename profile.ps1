dc6c075c836702e7b68828c4668e6eb77292b66e
fracsinus <fracsinus@gmail.com>
2021-07-23 00:54:41 +0900

    Add more shortcuts

--- profile.ps1
+++ profile.ps1
@@ -10,8 +10,10 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 Set-PSReadLineKeyHandler -key Ctrl+d -function ViExit  # Ctrl+d: exits the shell
-Set-PSReadLineKeyHandler -key Ctrl+u -function BackwardKillLine  # Ctrl+u: delete from the beginning of the line to the cursor
 Set-PSReadLineKeyHandler -key Ctrl+a -function BeginningOfLine  # Ctrl+a: move the cursor to the beginning of the line
 Set-PSReadLineKeyHandler -key Ctrl+e -function EndOfLine  # Ctrl+e: move the cursor to the end of the line
+Set-PSReadLineKeyHandler -key Alt+Backspace -function BackwardDeleteWord  # Ctrl+Backspace: delete the previous word
+Set-PSReadLineKeyHandler -key Ctrl+u -function BackwardKillLine  # Ctrl+u: delete from the beginning of the line to the cursor
+Set-PSReadLineKeyHandler -key Ctrl+y -function Yank  # Ctrl+y: yank(paste) the most recently killed text
 
 . $PSScriptRoot\aliases.ps1  # Load aliases
