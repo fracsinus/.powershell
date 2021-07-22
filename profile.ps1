efdbde459c00b72484891acc71c9cfe80a7522d7
fracsinus <fracsinus@gmail.com>
2021-07-23 00:41:34 +0900

    Add configs

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,15 @@
 # %USERPROFILE%\Documents\WindowsPowerShell\profile.ps1
-# Sign before use (Set-AuthenticodeSignature)
-Set-PSReadLineKeyHandler -key Ctrl+d -function ViExit  # Ctrl+d to quit shell
+# If Get-ExecutionPolicy is 'AllSigned', sign this script before use (Set-AuthenticodeSignature)
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+ Import-Module "$ChocolateyProfile"
+}
+
+Set-PSReadLineKeyHandler -key Ctrl+d -function ViExit  # Ctrl+d: exits the shell
+# Set-PSReadLineKeyHandler -key Ctrl+u -function ViYankBeginningOfLine  # Ctrl+d to quit shell
+Set-PSReadLineKeyHandler -key Ctrl+a -function BeginningOfLine  # Ctrl+a: move cursor to the beginning of the line
+Set-PSReadLineKeyHandler -key Ctrl+e -function EndOfLine  # Ctrl+e: move cursor to the end of the line
+
+. $PSScriptRoot\aliases.ps1  # Load aliases
